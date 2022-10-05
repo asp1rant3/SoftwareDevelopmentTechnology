96e23277a57a5bcc7c221057d0df58f106351c6f
Aleksandr <asp1rantzenevich@gmail.com>
2022-10-06 02:43:15 +0300

    #

--- cources/course_3/Labs/Lab21/Program.cs
+++ cources/course_3/Labs/Lab21/Program.cs
@@ -39,6 +39,47 @@ namespace Lab21
         }
     }
 
+    // Класс Одиночка предоставляет метод `GetInstance`, который ведёт себя как
+    // альтернативный конструктор и позволяет клиентам получать один и тот же
+    // экземпляр класса при каждом вызове.
+
+    // EN : The Singleton should always be a 'sealed' class to prevent class
+    // inheritance through external classes and also through nested classes.
+    public sealed class Singleton
+    {
+        // Конструктор Одиночки всегда должен быть скрытым, чтобы предотвратить
+        // создание объекта через оператор new.
+        private Singleton() { }
+
+        // Объект одиночки храниться в статичном поле класса. Существует
+        // несколько способов инициализировать это поле, и все они имеют разные
+        // достоинства и недостатки. В этом примере мы рассмотрим простейший из
+        // них, недостатком которого является полная неспособность правильно
+        // работать в многопоточной среде.
+        private static Singleton _instance;
+
+        // Это статический метод, управляющий доступом к экземпляру одиночки.
+        // При первом запуске, он создаёт экземпляр одиночки и помещает его в
+        // статическое поле. При последующих запусках, он возвращает клиенту
+        // объект, хранящийся в статическом поле.
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+            return _instance;
+        }
+
+        // Наконец, любой одиночка должен содержать некоторую бизнес-логику,
+        // которая может быть выполнена на его экземпляре.
+        public void someBusinessLogic()
+        {
+            // ...
+        }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
@@ -75,6 +116,19 @@ namespace Lab21
             DisplayValues(p2);
             Console.WriteLine("   p3 instance values (everything was kept the same):");
             DisplayValues(p3);
+
+            // Клиентский код.
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("Singleton works, both variables contain the same instance.");
+            }
+            else
+            {
+                Console.WriteLine("Singleton failed, variables contain different instances.");
+            }
         }
 
         public static void DisplayValues(Person p)
